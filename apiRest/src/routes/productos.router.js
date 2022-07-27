4ca6fd0e59df25146a31474c61b84055ca653641
maxiamin22 <109312229+maxiamin22@users.noreply.github.com>
2022-07-27 16:23:50 -0300

    Add files via upload

---
+++ apiRest/src/routes/productos.router.js
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import Manager from "../Manager/contenedor.js"
+const manager  = new Manager();
+const router = Router();
+
+///GET '/api/productos' -> devuelve todos los productos.
+
+router.get('/',async(req,res)=>{
+    let obtenerTodo = await manager.getAll()
+    res.send(obtenerTodo);
+})
+
+//GET '/api/productos/:id' -> devuelve un producto según su id.
+
+router.get('/:id',async(req,res)=>{
+    let Lista = await manager.getAll()
+    if (req.params.id >Lista.length) {
+        res.send("404 El valor pedido no existe")
+    } else {
+        let numero = req.params.id
+        let obtenerId = await manager.getById(numero)
+        res.send(obtenerId)
+    }
+
+})
+
+//POST '/api/productos' -> recibe y agrega un producto.
+
+router.post('/:id',async(req,res)=>{
+    let producto = req.params
+    res.send({status:"succes", message:"Product Added"})
+    await manager.save(producto)
+})
+
+//PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+
+router.put('/:id',async(req,res)=>{
+    let producto = req.params.id
+   await manager.actualizar(producto)
+   res.send("actualizado")
+})
+
+
+//DELETE '/api/productos/:id' -> elimina un producto según su id.
+router.delete('/:id', async(req, res) => {
+    let id = req.params
+    res.send("eliminado")
+    await manager.deleteById(id.delete)
+    
+    
+})
+
+
+
+
+export default router;
